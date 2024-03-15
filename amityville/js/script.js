301cb23ca72d381b5e0f9843f215f5d47d7b2507
anderasinggih <anderasinggih1@gmail.com>
2024-03-16 02:47:29 +0700

    web amityville

---
+++ amityville/js/script.js
@@ -0,0 +1,18 @@
+//Toggle Class Active
+const navbarNav = document.querySelector(".navbar-nav");
+
+//ketika hamburger di klik
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//klik di luar sidebar untuk hide nav
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+
